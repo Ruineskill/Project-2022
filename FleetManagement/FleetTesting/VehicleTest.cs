49ed3e01b81ad2f4bc848d95fc60e1fbc96d5c3c
ProjectGK <gio.kavel@gmail.com>
2022-03-02 18:45:22 +0100

    Created test classes

---
+++ FleetManagement/FleetTesting/VehicleTest.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+
+namespace FleetTesting
+{
+    public class VehicleTest
+    {
+        [Fact]
+        public void DeleteVehicle()
+        {
+
+        }
+
+        [Fact]
+        public void AddVehicle()
+        {
+
+        }
+
+        [Fact]
+        public void ModifyVehicle()
+        {
+
+        }
+
+       
+
+        [Fact]  // check if duplication of license plate is disallowed
+        public void LicensePlateCheck()
+        {
+
+        }
+
+        
+
+        [Fact]  // Check if duplication of chassisnumber is disallowed
+        public void ChassisNumberCheck()
+        {
+
+        }
+    }
+}
