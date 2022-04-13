2a6015b39cbe5d83dabae7e48ee014fe8d543abd
ProjectGK <gio.kavel@gmail.com>
2022-04-14 00:00:09 +0200

    update

--- FleetManagement/RestAPI/Program.cs
+++ FleetManagement/RestAPI/Program.cs
@@ -1,30 +1,143 @@
+#nullable disable
 using Domain.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Repository.Contexts;
+using Repository.DBInitializers;
 using Repository.Repositories;
+using RestAPI.Services;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "RestAPI", Version = "v1" });
+    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
 
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
+
+// database
+var connectionString = builder.Configuration.GetConnectionString("RestAPIConnection");
+builder.Services.AddDbContext<Context>(options => options.UseSqlServer(connectionString),
+                                       ServiceLifetime.Transient, ServiceLifetime.Singleton);
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.SignIn.RequireConfirmedAccount =true;
+    options.Password.RequireDigit = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 3;
+}).AddEntityFrameworkStores<Context>().AddDefaultTokenProviders();
+
+
+
+// Authentication
+builder.Services.AddAuthentication(auth =>
+{
+    auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidAudience = builder.Configuration["AuthSettings:Audience"],
+        ValidIssuer = builder.Configuration["AuthSettings:Issuer"],
+        RequireExpirationTime = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["AuthSettings:Key"])),
+        ValidateIssuerSigningKey = true,
+        SaveSigninToken = true,
+
+    };
+});
+
+
+// Authorization
+builder.Services.AddAuthorization(options =>
+{
+    options.FallbackPolicy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+});
+
+
+// Repositories
 builder.Services.AddSingleton<ICarRepository, CarRepository>();
 builder.Services.AddSingleton<IFuelCardRepository, FuelCardRepository>();
 builder.Services.AddSingleton<IPersonRepository, PersonRepository>();
 
 
+// services
+builder.Services.AddScoped<IUserService, UserService>();
+
+
 var app = builder.Build();
 
+// data seed
+using(var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var userManager = services.GetService<UserManager<IdentityUser>>();
+    var roleManager = services.GetService<RoleManager<IdentityRole>>();
+    UserInitializer.SeedData(userManager, roleManager);
+
+    using var context = services.GetService<Context>();
+    CarInitializer.SeedData(context);
+    FuelCardInitializer.SeedData(context);
+    PersonInitializer.SeedData(context);
+
+}
+
+
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if(app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
