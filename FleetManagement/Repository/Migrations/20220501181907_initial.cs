ea336eb9ebb4fa3a810ecab054d650195893fb5f
ProjectGK <gio.kavel@gmail.com>
2022-05-01 20:49:19 +0200

    imigration/update for context changes

---
+++ FleetManagement/Repository/Migrations/20220501181907_initial.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FuelCards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CardNumber = table.Column<long>(type: "bigint", nullable: false),
+                    ExpirationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PinCode = table.Column<int>(type: "int", nullable: false),
+                    UsableFuelTypes = table.Column<int>(type: "int", nullable: false),
+                    Blocked = table.Column<bool>(type: "bit", nullable: false),
+                    Delete = table.Column<bool>(type: "bit", nullable: false),
+                    PersonId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FuelCards", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    NationalRegistrationNumber = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    DrivingLicenseType = table.Column<int>(type: "int", nullable: false),
+                    Address_Street = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address_Number = table.Column<int>(type: "int", nullable: true),
+                    Address_City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address_ZipCode = table.Column<int>(type: "int", nullable: true),
+                    FuelCardId = table.Column<int>(type: "int", nullable: true),
+                    Delete = table.Column<bool>(type: "bit", nullable: false),
+                    CarId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Persons_FuelCards_FuelCardId",
+                        column: x => x.FuelCardId,
+                        principalTable: "FuelCards",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Brand = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ChassisNumber = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    LicensePlate = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    FuelType = table.Column<int>(type: "int", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    PersonId = table.Column<int>(type: "int", nullable: true),
+                    Color = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NumberOfDoors = table.Column<int>(type: "int", nullable: false),
+                    Delete = table.Column<bool>(type: "bit", nullable: false),
+                    RequiredLicence = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cars_Persons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "Persons",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_ChassisNumber",
+                table: "Cars",
+                column: "ChassisNumber",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_LicensePlate",
+                table: "Cars",
+                column: "LicensePlate",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_PersonId",
+                table: "Cars",
+                column: "PersonId",
+                unique: true,
+                filter: "[PersonId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FuelCards_CardNumber",
+                table: "FuelCards",
+                column: "CardNumber",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Persons_FuelCardId",
+                table: "Persons",
+                column: "FuelCardId",
+                unique: true,
+                filter: "[FuelCardId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Persons_NationalRegistrationNumber",
+                table: "Persons",
+                column: "NationalRegistrationNumber",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+
+            migrationBuilder.DropTable(
+                name: "Persons");
+
+            migrationBuilder.DropTable(
+                name: "FuelCards");
+        }
+    }
+}
--- FleetManagement/Repository/Migrations/Auth/20220501181919_initial.cs
+++ FleetManagement/Repository/Migrations/Auth/20220501181919_initial.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace Repository.Migrations
+namespace Repository.Migrations.Auth
 {
     public partial class initial : Migration
     {
@@ -40,25 +39,6 @@ namespace Repository.Migrations
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_AspNetUsers", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "FuelCards",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    CardNumber = table.Column<long>(type: "bigint", nullable: false),
-                    ExpirationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    PinCode = table.Column<int>(type: "int", nullable: false),
-                    UsableFuelTypes = table.Column<int>(type: "int", nullable: false),
-                    Blocked = table.Column<bool>(type: "bit", nullable: false),
-                    Delete = table.Column<bool>(type: "bit", nullable: false),
-                    PersonId = table.Column<int>(type: "int", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_FuelCards", x => x.Id);
                 });
 
             migrationBuilder.CreateTable(
@@ -167,63 +147,6 @@ namespace Repository.Migrations
                         onDelete: ReferentialAction.Cascade);
                 });
 
-            migrationBuilder.CreateTable(
-                name: "Persons",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    NationalRegistrationNumber = table.Column<string>(type: "nvarchar(450)", nullable: false),
-                    DrivingLicenseType = table.Column<int>(type: "int", nullable: false),
-                    Address_Street = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Address_Number = table.Column<int>(type: "int", nullable: true),
-                    Address_City = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Address_ZipCode = table.Column<int>(type: "int", nullable: true),
-                    FuelCardId = table.Column<int>(type: "int", nullable: true),
-                    Delete = table.Column<bool>(type: "bit", nullable: false),
-                    CarId = table.Column<int>(type: "int", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Persons", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Persons_FuelCards_FuelCardId",
-                        column: x => x.FuelCardId,
-                        principalTable: "FuelCards",
-                        principalColumn: "Id");
-                });
-
-            migrationBuilder.CreateTable(
-                name: "Cars",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Brand = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Model = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    ChassisNumber = table.Column<string>(type: "nvarchar(450)", nullable: false),
-                    LicensePlate = table.Column<string>(type: "nvarchar(450)", nullable: false),
-                    FuelType = table.Column<int>(type: "int", nullable: false),
-                    Type = table.Column<int>(type: "int", nullable: false),
-                    PersonId = table.Column<int>(type: "int", nullable: true),
-                    Color = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    NumberOfDoors = table.Column<int>(type: "int", nullable: false),
-                    Delete = table.Column<bool>(type: "bit", nullable: false),
-                    RequiredLicence = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Cars", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Cars_Persons_PersonId",
-                        column: x => x.PersonId,
-                        principalTable: "Persons",
-                        principalColumn: "Id");
-                });
-
             migrationBuilder.CreateIndex(
                 name: "IX_AspNetRoleClaims_RoleId",
                 table: "AspNetRoleClaims",
@@ -262,44 +185,6 @@ namespace Repository.Migrations
                 column: "NormalizedUserName",
                 unique: true,
                 filter: "[NormalizedUserName] IS NOT NULL");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Cars_ChassisNumber",
-                table: "Cars",
-                column: "ChassisNumber",
-                unique: true);
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Cars_LicensePlate",
-                table: "Cars",
-                column: "LicensePlate",
-                unique: true);
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Cars_PersonId",
-                table: "Cars",
-                column: "PersonId",
-                unique: true,
-                filter: "[PersonId] IS NOT NULL");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_FuelCards_CardNumber",
-                table: "FuelCards",
-                column: "CardNumber",
-                unique: true);
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Persons_FuelCardId",
-                table: "Persons",
-                column: "FuelCardId",
-                unique: true,
-                filter: "[FuelCardId] IS NOT NULL");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Persons_NationalRegistrationNumber",
-                table: "Persons",
-                column: "NationalRegistrationNumber",
-                unique: true);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
@@ -320,19 +205,10 @@ namespace Repository.Migrations
                 name: "AspNetUserTokens");
 
             migrationBuilder.DropTable(
-                name: "Cars");
-
-            migrationBuilder.DropTable(
                 name: "AspNetRoles");
 
             migrationBuilder.DropTable(
                 name: "AspNetUsers");
-
-            migrationBuilder.DropTable(
-                name: "Persons");
-
-            migrationBuilder.DropTable(
-                name: "FuelCards");
         }
     }
 }
