c8fb5bad7beb75a640d5f1af986ec34c3bcd6b20
ProjectGK <gio.kavel@gmail.com>
2022-05-01 20:45:24 +0200

    context for authentication database

---
+++ FleetManagement/Repository/Contexts/AuthContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Repository.EntityTypeConfigurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Contexts
+{
+    public class AuthContext : IdentityDbContext
+    {
+        public AuthContext(DbContextOptions<AuthContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserEntityTypeConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
