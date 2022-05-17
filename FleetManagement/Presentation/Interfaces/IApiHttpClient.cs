eee273be7b887c803b0aa0526d03ee265c0d9047
ProjectGK <gio.kavel@gmail.com>
2022-05-17 17:08:16 +0200

    return type update

--- FleetManagement/Presentation/Interfaces/IApiHttpClient.cs
+++ FleetManagement/Presentation/Interfaces/IApiHttpClient.cs
@@ -7,10 +7,9 @@ namespace Presentation.HttpClients
     public interface IApiHttpClient
     {
         Task<T> GetAsync<T>(string uri);
-        Task<T> PostAsync<T>(string uri, T payLoad);
-        Task<T> PostAsync<T,O>(string uri, O payLoad);
-        Task<T> PutAsync<T>(string uri, T payLoad);
-        Task<HttpStatusCode> DeleteAsync(string uri, StringContent payLoad);
+        Task<HttpStatusCode> PostAsync<T>(string uri, T payLoad);
+        Task<R> PostAsync<R,T>(string uri, T payLoad);
+        Task<HttpStatusCode> PutAsync<T>(string uri, T payLoad);
         Task<HttpStatusCode> DeleteAsync(string uri);
     }
 }
