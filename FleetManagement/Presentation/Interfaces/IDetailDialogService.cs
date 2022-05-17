bbac35abef431d3abf436d31932b80a5a834b42e
ProjectGK <gio.kavel@gmail.com>
2022-05-17 18:22:52 +0200

    Details navigation and dialog

---
+++ FleetManagement/Presentation/Interfaces/IDetailDialogService.cs
@@ -0,0 +1,19 @@
+﻿using Presentation.ViewModels.Bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Interfaces
+{
+    public interface IDetailDialogService
+    {
+        void SetContent(DetailViewModelBase dtail);
+
+        void Show();
+
+        void Close();
+
+    }
+}
