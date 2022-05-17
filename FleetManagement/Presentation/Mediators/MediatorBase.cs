107dbf13723c01f1b3d7f4d9730e69d07b4f9e17
ProjectGK <gio.kavel@gmail.com>
2022-05-17 18:22:05 +0200

    mediator base

---
+++ FleetManagement/Presentation/Mediators/MediatorBase.cs
@@ -0,0 +1,31 @@
+﻿#nullable disable warnings
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Mediators
+{
+    public class MediatorBase<T>
+    {
+        public event Action<T> Created;
+        public event Action<T> Updated;
+        public event Action<T> Deleted;
+
+        public void Create(T obj)
+        {
+            Created?.Invoke(obj);
+        }
+
+        public void Update(T obj)
+        {
+            Updated?.Invoke(obj);
+        }
+
+        public void Delete(T obj)
+        {
+            Deleted?.Invoke(obj);
+        }
+    }
+}
