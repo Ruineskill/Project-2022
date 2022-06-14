4b7efa5198aa7cfe637c417aefa2f04243afc64a
ProjectGK <gio.kavel@gmail.com>
2022-06-14 17:20:14 +0200

    viewmodel used as context in selection dialog

---
+++ FleetManagement/Presentation/ViewModels/Dialogs/SelectorDialogViewModel.cs
@@ -0,0 +1,53 @@
+﻿
+using Presentation.Interfaces.Listing;
+using Presentation.ViewModels.Bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.ComponentModel;
+using System.Windows.Data;
+
+namespace Presentation.ViewModels.Dialogs
+{
+    public class SelectorDialogViewModel : ViewModelBase
+    {
+        public ViewModelBase? Selected { get; set; }
+
+        public IListingService? _listing;
+        public IListingService? Listing
+        {
+            get => _listing;
+            set
+            {
+                
+                _listing = value;
+
+                //Items = CollectionViewSource.GetDefaultView(Listing?.Items);
+                //Items.Filter = c => ((CarViewModel)c).FuelType == Domain.Models.Enums.FuelType.Diesel;
+
+
+                //_listing.View.Filter = c => ((CarViewModel)c).FuelType == Domain.Models.Enums.FuelType.Diesel;
+
+                OnPropertyChanged(nameof(Items));
+            }
+        }
+
+        public ICollectionView? Items => _listing?.View;
+
+
+        public ICommand? SelectCommand { get; set; }
+
+        public ICommand? CancelCommand { get; set; }
+
+        private string _search = string.Empty;
+        public string Search
+        {
+            get => _search;
+            set => SetProperty(ref _search, value);
+        }
+
+    }
+}
