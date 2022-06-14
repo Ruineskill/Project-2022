049e19f20ce92a5f83ec0b3cc9b17eecab64a901
ProjectGK <gio.kavel@gmail.com>
2022-06-14 17:04:16 +0200

    Interface for all mediator operations

---
+++ FleetManagement/Presentation/Interfaces/IFleetMediator.cs
@@ -0,0 +1,19 @@
+﻿using Presentation.DTO;
+using Presentation.ViewModels;
+using Presentation.ViewModels.Bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Interfaces
+{
+    public interface IFleetMediator
+    {
+        void Create(ViewModelBase obj);
+        void Update(ViewModelBase obj);
+        void Delete(ViewModelBase obj);
+
+    }
+}
