9c9d0fde31116b005c191f5b26e711e563c2a312
ProjectGK <gio.kavel@gmail.com>
2022-06-14 17:15:11 +0200

    service for displaying message to user

---
+++ FleetManagement/Presentation/Services/MessageService.cs
@@ -0,0 +1,32 @@
+﻿using Presentation.Interfaces;
+using System.Threading.Tasks;
+using System.Windows;
+using MaterialDesignThemes.Wpf;
+using Presentation.Views.Dialogs;
+
+namespace Presentation.Services
+{
+    public class MessageService : IMessageService
+    {
+        public async Task DisplayErrorAsync(string message)
+        {
+            var dialog = new ConfirmationDialog(message);
+            dialog.CancelButton.Visibility = Visibility.Hidden;
+            dialog.MsgIcon.Kind = PackIconKind.ErrorOutline;
+            await DialogHost.Show(dialog, "MainWinDialog");
+        }
+
+
+        public async Task<bool> DisplayWarningAsync(string message, string hostName)
+        {
+            var dialog = new ConfirmationDialog(message);
+            dialog.CancelButton.Content = "No";
+            dialog.OkButton.Content = "Yes";
+            var result = await DialogHost.Show(dialog, hostName);
+
+            if(result != null) return (bool)result;
+            return false;
+        }
+
+    }
+}
