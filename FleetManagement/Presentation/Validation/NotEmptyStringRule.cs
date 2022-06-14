14404bd343986423a5ad6840575f879f6eb0593d
ProjectGK <gio.kavel@gmail.com>
2022-06-14 17:16:05 +0200

    validation rule for empty string

---
+++ FleetManagement/Presentation/Validation/NotEmptyStringRule.cs
@@ -0,0 +1,30 @@
+﻿#nullable disable warnings
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows.Controls;
+
+namespace Presentation.Validation
+{
+    class NotEmptyStringRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var result = value?.ToString();
+            if(string.IsNullOrWhiteSpace(result))
+            {
+                return new ValidationResult(false, "Required field");
+            }
+
+            if(result.Any(char.IsDigit))
+            {
+                return new ValidationResult(false, "Numbers are not allowed");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
