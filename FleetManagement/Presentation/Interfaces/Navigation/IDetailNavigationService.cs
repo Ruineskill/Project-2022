061598694422e237125bb2bca422f12876c19d3c
ProjectGK <gio.kavel@gmail.com>
2022-06-14 17:09:06 +0200

    interface for detail dialog navigation maybe unused

---
+++ FleetManagement/Presentation/Interfaces/Navigation/IDetailNavigationService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Interfaces.Navigation
+{
+    public interface IDetailNavigationService : INavigationService
+    {
+        void GoBack();
+    }
+}
