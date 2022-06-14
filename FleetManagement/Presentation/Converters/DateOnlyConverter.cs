0d17972734c33c7306dca10c950c9967d6d96455
ProjectGK <gio.kavel@gmail.com>
2022-06-14 16:58:49 +0200

    converts dateOnly to datetime for wpf controls

---
+++ FleetManagement/Presentation/Converters/DateOnlyConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Presentation.Converters
+{
+    public class DateOnlyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value == null) return DateTime.Now;
+
+            var dateOnly = (DateOnly)value;
+
+            DateTime? datetime = dateOnly.ToDateTime(TimeOnly.MinValue, DateTimeKind.Local);
+
+            return datetime;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+
+
+            if(value == null) return DateOnly.FromDateTime(DateTime.Now);
+
+            var date = (DateTime)value;
+
+            return DateOnly.FromDateTime(date);
+        }
+    }
+}
