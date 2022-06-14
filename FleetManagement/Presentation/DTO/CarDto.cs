49a692969b056587ac7764a98638746dafb98ff4
ProjectGK <gio.kavel@gmail.com>
2022-06-14 16:59:57 +0200

    Car DTO

---
+++ FleetManagement/Presentation/DTO/CarDto.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.DTO
+{
+    public class CarDto
+    {
+
+        public int Id { get;  set; }
+        public string Brand { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public string ChassisNumber { get; set; } = string.Empty;
+        public string LicensePlate { get; set; } = string.Empty;
+        public FuelType FuelType { get ; set ; }
+        public CarType Type { get; set; }
+        public PersonDto? Person { get; set; } = null;
+        public string Color { get; set; } = string.Empty;
+        public int NumberOfDoors { get ; set; }
+        public DrivingLicenseType RequiredLicence { get ; set ; }
+    }
+}
