60bcccaa8d6f5f9dac994c4e70b168a34703dea3
ProjectGK <gio.kavel@gmail.com>
2022-06-14 17:09:31 +0200

    interface for main navigation

---
+++ FleetManagement/Presentation/Interfaces/Navigation/INavigationService.cs
@@ -0,0 +1,16 @@
+﻿using Presentation.ViewModels.Bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Interfaces.Navigation
+{
+    public interface INavigationService
+    {
+        ViewModelBase CurrentViewModel { get; }
+        void Navigate(ViewModelBase viewModel);
+        void OnViewModelChanged(Action currentViewModelChanged);
+    }
+}
