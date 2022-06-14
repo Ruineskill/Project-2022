af7aa103f0af141d63eb87034798c5ede4c600c3
ProjectGK <gio.kavel@gmail.com>
2022-06-14 17:00:08 +0200

    FuelCard DTO

---
+++ FleetManagement/Presentation/DTO/FuelCardDto.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Models.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace Presentation.DTO
+{
+    public class FuelCardDto
+    {
+        public int Id { get; set; }
+        public long CardNumber { get; set; }
+        public DateOnly ExpirationDate { get; set; }
+        public int PinCode { get; set; }
+        public ICollection<FuelType> UsableFuelTypes { get; set; } = new List<FuelType>();
+
+        public PersonDto? Person { get; set; }
+
+        public bool Blocked { get; set; }
+    }
+}
