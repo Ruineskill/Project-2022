d263780977af397a9c5a43a2dc69284a2355fb77
ProjectGK <gio.kavel@gmail.com>
2022-06-14 16:59:46 +0200

    Address DTO

---
+++ FleetManagement/Presentation/DTO/AddressDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.DTO
+{
+    public class AddressDto
+    {
+        public string Street { get; set; } = string.Empty;
+
+        public int Number { get; set; }
+
+        public string City { get; set; } = string.Empty;
+
+        public int ZipCode { get; set; }
+
+    }
+}
